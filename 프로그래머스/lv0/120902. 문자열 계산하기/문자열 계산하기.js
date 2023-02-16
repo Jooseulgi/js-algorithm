59937935084b027f4ced2f450e57416234bfbaf9
주슬기 <54945205+Jooseulgi@users.noreply.github.com>
2023-02-17 01:35:58 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,15 +1,29 @@
 function solution(my_string) {
-    const stack = [];
-    const arr = my_string.split(' ')
-    let sign = 1;
-    arr.forEach(item => {
-        if (item === "+") {
-            sign = 1;
-        } else if (item === "-") {
-            sign = -1;
-        } else {
-            stack.push(item * sign);
+     const str = my_string.split(" ");
+    let result = Number(str[0]);
+     str.forEach((s, index) => {
+        if(s === "+"){
+            result += Number(str[index + 1]);
+        }
+
+        if(s === "-"){
+            result -= Number(str[index + 1]);
         }
     })
-    return stack.reduce((a,b) => a + b, 0);
+
+    return result;
+//     const stack = [];
+//     const arr = my_string.split(' ')
+//     let sign = 1;
+    
+//     arr.forEach(item => {
+//         if (item === "+") {
+//             sign = 1;
+//         } else if (item === "-") {
+//             sign = -1;
+//         } else {
+//             stack.push(item * sign);
+//         }
+//     })
+//     return stack.reduce((a,b) => a + b, 0);
 }
