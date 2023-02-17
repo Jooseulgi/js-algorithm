9f2716d55168bc9c6c173ecc2b34cde15a3509e1
주슬기 <54945205+Jooseulgi@users.noreply.github.com>
2023-02-17 23:51:48 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.13 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -3,10 +3,10 @@ function solution(s) {
     let zeroNum = 0
     while( s.length > 1) {
         count += 1
-        sLen = s.length;
-        s = s.split("0").join("");
-        zeroNum += (sLen - s.length);
-        s = s.length.toString(2);
+        sLen = s.length
+        s = s.split("0").join("")
+        zeroNum += (sLen - s.length)
+        s = s.length.toString(2)
     }
     return [count,zeroNum]
 }
